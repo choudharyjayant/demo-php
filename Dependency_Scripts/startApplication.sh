ddb8ea069f6486c1a1e16e1c2b08a4b1ba0d8999
Tushar Khachane <61828288+tushar8871@users.noreply.github.com>
2021-08-05 09:36:38 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/php/
-composer install
-screen -d -m -S SERVER bash -c "php -S 0.0.0.0:8000 -t web/"
-#nohup php -S 0.0.0.0:8000 -t web/ &
+sudo composer install
+#screen -d -m -S SERVER bash -c "php -S 0.0.0.0:8000 -t web/"
+nohup php -S 0.0.0.0:8000 -t web/ &
